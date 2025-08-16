7f914a98efb6a42ba911a4e9110b59f91804627a
Sosank Sarmah <sosanksarmah@gmail.com>
2025-08-16 07:21:41 +0530

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import{
+import {
   getAuth,
   sendPasswordResetEmail,
   signInWithEmailAndPassword
@@ -12,7 +11,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAnOrm7pHWFjQy5hk8x8VtXTMZmAplaFlM",
   authDomain: "cylo-b89dc.firebaseapp.com",
   projectId: "cylo-b89dc",
-  storageBucket: "cylo-b89dc.appspot.com", // Corrected the storage bucket URL
+  storageBucket: "cylo-b89dc.appspot.com",
   messagingSenderId: "451560803879",
   appId: "1:451560803879:web:3ae36b338e7a0056294802"
 };
@@ -23,7 +22,7 @@ export const auth = getAuth(app);
 
 // Reset password
 const Reset = document.getElementById("reset");
-Reset.addEventListener("click", function(event){
+Reset.addEventListener("click", function (event) {
   event.preventDefault();
 
   const email = document.getElementById("email").value;
@@ -32,8 +31,6 @@ Reset.addEventListener("click", function(event){
       alert("Password reset email sent!");
     })
     .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert("error: " + errorCode + " - " + errorMessage);
-      });
-  });
+      alert("error: " + error.code + " - " + error.message);
+    });
+});
